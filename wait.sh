183b71949ee8afa7f9d78d44737166bc2e0ed480
Asaf Greenholts <asaf@cidersecurity.io>
2022-12-12 13:47:38 +0200

    fix wait script

--- wait.sh
+++ wait.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in {1..120}
 do
-  cmd=`docker logs gitlab | grep 'GitLab is ready!'`
-  if [ $? -eq 0 ]; then
+  gitlab_status_code=$(curl --write-out %{http_code} --silent --output /dev/null "localhost:4000/api/v4/user?access_token=998b5802ec365e17665d832f3384e975")
+  if [ "$gitlab_status_code" -eq 200 ]; then
     echo break
     break
   fi
